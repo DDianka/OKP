fcb0de6042432da7b9d9c5c5f2280feaa4071629
Diana <ddianka.1917@gmail.com>
2019-09-07 22:20:20 +0300

    secTask

--- ConsoleApplication1/homework.7-sept-2019.cpp
+++ ConsoleApplication1/homework.7-sept-2019.cpp
@@ -4,40 +4,47 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void log(int x, int y) {
+	cout << "x: " << x << ", y: " << y << endl;
+}
 
-	int x = 2, y = 3;
-	/* оепбши яоняна
-	int z;
-	z = x; //2
+void firstWay() {
+	int x = 2, y = 3, z;
+
+	z = x; 
 	x = y;
 	y = z;
-	cout << x << endl;
-	cout << y;
-	*/
-	/* брнпни
+	log(x, y);
+}
+
+void secondWay() {
+	int x = 2, y = 3;
+	
 	swap(x, y);
-	cout << x << endl;
-	cout << y;
-	*/
-	/*
+	log(x, y);
+}
+
+void thirdWay() {
+	int x = 2, y = 3;
+
 	x = x + y;
 	y = y - x;
 	y = -y;
 	x = x - y;
-	cout << x << endl;
-	cout << y;
-	*/
+	log(x, y);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void getLastNumber(int number) {
+	cout << number % 10;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main() {
+	// firstWay();
+	// secondWay();
+	// thirdWay();
+
+	// getLastNumber(321);
+
+
+}
 
