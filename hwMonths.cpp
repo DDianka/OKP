df24a5102dc450b2a018c531efb6931e576788b9
unknown <doroginalexanderqt@gmail.com>
2019-09-15 11:37:54 +0300

    hw for OKP (switch-case month) v1

---
+++ hwMonths.cpp
@@ -0,0 +1,22 @@
+﻿// hwApp2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int month;
+	cout << "Enter month number: " << endl;
+	cin >> month;
+	switch (month) {
+		case 12: case 1: case 2: cout << "Winter"; break;
+		case 3: case 4: case 5: cout << "Spring"; break;
+		case 6: case 7: case 8: cout << "Summer"; break;
+		case 9: case 10: case 11: cout << "Autumn"; break;
+		default: cout << "Wrong input"; break;
+	}
+}
+
+
